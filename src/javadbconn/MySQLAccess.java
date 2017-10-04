52c600df29e0f797b6f99e446be19e98e8d9d822
RaviGarbujaPun <cbpmgr@gmail.com>
2017-10-04 23:14:07 +0545

    Add phones to ArrayList via constructor and print them on console

--- src/javadbconn/MySQLAccess.java
+++ src/javadbconn/MySQLAccess.java
@@ -11,16 +11,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  * @author ravi
  */
 public class MySQLAccess {
+
     private Connection connect = null;
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
+    ArrayList<Phone> phoneArrList = new ArrayList<>();
 
     public void readDataBase() throws Exception {
         try {
@@ -28,7 +33,7 @@ public class MySQLAccess {
             Class.forName("com.mysql.jdbc.Driver");
             // Setup the connection with the DB
             connect = DriverManager
-                    .getConnection("jdbc:mysql://localhost/phone_iinfo","root","");
+                    .getConnection("jdbc:mysql://localhost/phone_iinfo", "root", "");
 
             // Statements allow to issue SQL queries to the database
             statement = connect.createStatement();
@@ -38,7 +43,7 @@ public class MySQLAccess {
             writeResultSet(resultSet);
         } catch (ClassNotFoundException | SQLException e) {
             //throw e;
-            System.out.println("Connection to datbase failed.");
+            System.out.println("Connection to database failed.");
         } finally {
             close();
         }
@@ -52,25 +57,38 @@ public class MySQLAccess {
         System.out.println("The columns in the table are: ");
 
         System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
-            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+        for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+            System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
         }
     }
 
     private void writeResultSet(ResultSet resultSet) throws SQLException {
+        
         // ResultSet is initially before the first data set
         while (resultSet.next()) {
             // It is possible to get the columns via name
             // also possible to get the columns via the column number
             // which starts at 1
             // e.g. resultSet.getSTring(2);
+            int id = resultSet.getInt("pid");
             String name = resultSet.getString("name");
             String price = resultSet.getString("price");
             String description = resultSet.getString("description");
-            System.out.println("Name: " + name);
-            System.out.println("Price: " + price);
-            System.out.println("Description: " + description);
+            String dateCreated = resultSet.getString("created_at");
+            //String dateUpdated = resultSet.getString("updated_at");
+            
+            //System.out.println(id+" "+name+" "+price+" "+description+" "+dateCreated); //this is working
+            
+
+            phoneArrList.add(new Phone(id, name, price, description, dateCreated));
         }
+
+        // Iterate over the List we just created with the enhanced for - the method will
+        // be called on all objects in the List.
+        for (Phone phones : phoneArrList) {
+            System.out.println(phones.getName());
+        }
+        
     }
 
     // You need to close the resultSet
@@ -88,8 +106,8 @@ public class MySQLAccess {
                 connect.close();
             }
         } catch (Exception e) {
-               
+
         }
     }
-    
+
 }
